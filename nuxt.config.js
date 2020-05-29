c252b8c2ad8c8f187a1fda3d87600fe980c9a9b4
Martin Retrou <martin.retrou@derniercri.io>
2020-05-30 00:32:05 +0200

    chore(pwa): add icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -70,8 +70,53 @@ export default {
    */
   pwa: {
     manifest: {
-      name: 'hw-nuxtjs-prismic',
-      lang: 'en'
+      name: 'SpaceX Launch Tracker',
+      lang: 'en',
+      orientation: 'portrait',
+      display: 'standalone',
+      theme_color: 'black',
+      icons: [
+        {
+          src: '/icons/icon-72x72.png',
+          sizes: '72x72',
+          type: 'image/png'
+        },
+        {
+          src: '/icons/icon-96x96.png',
+          sizes: '96x96',
+          type: 'image/png'
+        },
+        {
+          src: '/icons/icon-128x128.png',
+          sizes: '128x128',
+          type: 'image/png'
+        },
+        {
+          src: '/icons/icon-144x144.png',
+          sizes: '144x144',
+          type: 'image/png'
+        },
+        {
+          src: '/icons/icon-152x152.png',
+          sizes: '152x152',
+          type: 'image/png'
+        },
+        {
+          src: '/icons/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/icons/icon-384x384.png',
+          sizes: '384x384',
+          type: 'image/png'
+        },
+        {
+          src: '/icons/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
     }
   },
   /*
